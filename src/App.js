bb23375d904c19e27e969a37bcc5840788404a42
Thiago Santos <thiagoss1995@gmail.com>
2021-08-22 22:34:51 -0300

    Listando os repositórios em tela

--- src/App.js
+++ src/App.js
@@ -1,8 +1,18 @@
-import React from "react";
-
+import React, { useState, useEffect } from "react";
+import api from './services/api';
 import "./styles.css";
 
 function App() {
+
+  const [repositories, setRepositories] = useState([]);
+
+  //Preparação do componente para exibição em tela
+  useEffect(() => {
+    api.get('/repositories').then(res => {
+      setRepositories(res.data);
+    });
+  }, []);
+
   async function handleAddRepository() {
     // TODO
   }
@@ -13,14 +23,23 @@ function App() {
 
   return (
     <div>
+
+
       <ul data-testid="repository-list">
-        <li>
+        {repositories.map(repository =>
+            <li key={repository.id}> {repository.title}
+            <button onClick={() => handleRemoveRepository(1)}> Remover </button>
+            </li>
+          )
+        }
+
+        {/* <li>
           Repositório 1
 
           <button onClick={() => handleRemoveRepository(1)}>
             Remover
           </button>
-        </li>
+        </li> */}
       </ul>
 
       <button onClick={handleAddRepository}>Adicionar</button>
